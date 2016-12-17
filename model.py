8c93711f279f3a2dcd5590c742e485564b9ccf10
kadriansyah <kadriansayah@gmail.com>
2016-12-17 07:07:47 +0700

    adding language model

--- model.py
+++ model.py
@@ -2,21 +2,22 @@ import pickle
 import string
 import nltk
 from nltk.corpus import PlaintextCorpusReader
+from enum import Enum
 
 class LanguageModel:
-    CORPUS_PATH  = 'data/'
+    CORPUS_PATH  = 'data/clean/'
 
-    def __init__(self, is_read=False, corpus_path=CORPUS_PATH):
-        if is_read is False:
+    def __init__(self, load=False, corpus_path=CORPUS_PATH):
+        if load is False:
             self.words = self.__read_corpus(corpus_path)
             self.freq_dist = self.__freq_dist(self.words)
-            self.bigram_cfreq_dist = self.__bigram_conditional_freq_dist(self.words)
-            self.bigram_cprob_dist = self.__bigram_conditional_prob_dist(self.bigram_cfreq_dist)
+            self.cond_freq_dist = self.__cond_freq_dist(self.words)
+            self.cond_prob_dist = self.__cond_prob_dist(self.cond_freq_dist)
         else:
-            self.words = pickle.load( open( "pickled/_words.p", "rb" ) )
-            self.freq_dist = pickle.load( open( "pickled/_freq_dist.p", "rb" ) )
-            self.bigram_cfreq_dist = pickle.load( open( "pickled/_bigram_cfreq_dist.p", "rb" ) )
-            self.bigram_cprob_dist = pickle.load( open( "pickled/_bigram_cprob_dist.p", "rb" ) )
+            self.words = pickle.load(open("pickled/_words.p", "rb"))
+            self.freq_dist = pickle.load(open("pickled/_freq_dist.p", "rb"))
+            self.cond_freq_dist = pickle.load(open("pickled/_cond_freq_dist.p", "rb"))
+            self.cond_prob_dist = pickle.load(open("pickled/_cond_prob_dist.p", "rb"))
 
     def __read_corpus(self, corpus_path):
         wordlists = PlaintextCorpusReader(corpus_path, '.*', encoding='latin-1')
@@ -33,28 +34,24 @@ class LanguageModel:
     def __freq_dist(self, words):
         return nltk.FreqDist(words)
 
-    def __bigram_conditional_freq_dist(self, words):
+    def __cond_freq_dist(self, words):
         return nltk.ConditionalFreqDist(nltk.bigrams(words))
 
-    def __bigram_conditional_prob_dist(self, cfreq_dist):
-        return nltk.ConditionalProbDist(cfreq_dist, nltk.MLEProbDist)
+    def __cond_prob_dist(self, cond_freq_dist):
+        return nltk.ConditionalProbDist(cond_freq_dist, nltk.MLEProbDist)
 
-    def unigram_prob(self, word):
+    def __unigram_prob(self, word):
         return self.freq_dist[word] / len(self.words)
 
     def sentence_prob(self, sentence):
         words = sentence.split()
-        probability = 1
-        for idx, word in enumerate(words):
-            if idx == len(words) - 1:
-                break
-            probability *= self.bigram_cprob_dist[word].prob(words[idx + 1])
-
-        probability *= self.unigram_prob(words[0])
-        return probability
+        prob  = self.__unigram_prob(words[0])
+        for ii in range(1, len(words) - 1):
+            prob *= self.cond_prob_dist[words[ii]].prob(words[ii + 1])
+        return prob
 
     def save(self):
         pickle.dump( self.words, open( "pickled/_words.p", "wb" ) )
         pickle.dump( self.freq_dist, open( "pickled/_freq_dist.p", "wb" ) )
-        pickle.dump( self.bigram_cfreq_dist, open( "pickled/_bigram_cfreq_dist.p", "wb" ) )
-        pickle.dump( self.bigram_cprob_dist, open( "pickled/_bigram_cprob_dist.p", "wb" ) )
+        pickle.dump( self.cond_freq_dist, open( "pickled/_cond_freq_dist.p", "wb" ) )
+        pickle.dump( self.cond_prob_dist, open( "pickled/_cond_prob_dist.p", "wb" ) )
--- spell.py
+++ spell.py
@@ -3,67 +3,115 @@
 import os
 import re
 import csv
+import string
+import pickle
 from collections import Counter
+import model
 
-NEWLINE = '\n'
-SKIP_FILES = {'cmds'}
-CORPUS_PATH  = 'corpus/'
+class SpellCorrector:
 
-def read_files(path):
-    for root, dir_names, file_names in os.walk(path):
-        for path in dir_names:
-            read_files(os.path.join(root, path))
-        for file_name in file_names:
-            if file_name not in SKIP_FILES:
-                file_path = os.path.join(root, file_name)
-                if os.path.isfile(file_path):
-                    lines = []
-                    f = open(file_path, encoding='latin-1')
-                    for line in f:
-                        lines.append(line)
-                    f.close()
-                    content = NEWLINE.join(lines)
-                    yield file_path, content
+    NEWLINE = '\n'
+    SKIP_FILES = {'cmds'}
+    CORPUS_PATH  = 'data/clean/'
 
-def words(path):
-    words = []
-    for file_name, text in read_files(path):
-        print("process data => "+ file_name)
-        words += re.findall(r'\w+', text.lower())
-    return words
+    def __init__(self, load=False, corpus_path=CORPUS_PATH):
+        if load is False:
+            self.words = self.__words(corpus_path)
+            self.counter = self.__counter(self.words)
+            self.model = model.LanguageModel(load=True)
+        else:
+            self.words = pickle.load(open("pickled/_spell_words.p", "rb"))
+            self.counter = pickle.load(open("pickled/_spell_counter.p", "rb"))
+            self.model = model.LanguageModel(load=True)
 
-WORDS = Counter(words(CORPUS_PATH))
+    def __read_files(self, path):
+        for root, dir_names, file_names in os.walk(path):
+            for path in dir_names:
+                self.__read_files(os.path.join(root, path))
+            for file_name in file_names:
+                if file_name not in SpellCorrector.SKIP_FILES:
+                    file_path = os.path.join(root, file_name)
+                    if os.path.isfile(file_path):
+                        lines = []
+                        f = open(file_path, encoding='latin-1')
+                        for line in f:
+                            lines.append(line)
+                        f.close()
+                        content = SpellCorrector.NEWLINE.join(lines)
+                        yield file_path, content
 
-def P(word, N=sum(WORDS.values())):
-    "Probability of `word`."
-    return WORDS[word] / N
+    def __words(self, corpus_path):
+        words = []
+        for file_name, text in self.__read_files(corpus_path):
+            print("process data => "+ file_name)
+            words += re.findall(r'\w+', text.lower())
+        return words
 
-def correction(word):
-    "Most probable spelling correction for word."
-    return max(candidates(word), key=P)
+    def __counter(self, words):
+        return Counter(words)
 
-def candidates(word):
-    "Generate possible spelling corrections for word."
-    return (known([word]) | known(edits1(word)) | known(edits2(word)) | known(edits1Expanded(word)) | {word})
+    def __wordProb(self, word):
+        "Probability of `word`."
+        return self.counter[word] / sum(self.counter.values())
 
-def known(words):
-    "The subset of `words` that appear in the dictionary of WORDS."
-    return set(w for w in words if w in WORDS)
+    def correction(self, word):
+        "Most probable spelling correction for word."
+        return max(self.candidates(word), key=self.__wordProb)
 
-def edits1(word):
-    "All edits that are one edit away from `word`."
-    letters    = 'abcdefghijklmnopqrstuvwxyz'
-    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    # deletes    = [L + R[1:]               for L, R in splits if R]
-    # transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    # replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    inserts    = [L + c + R               for L, R in splits for c in letters]
-    # return set(deletes + transposes + replaces + inserts)
-    return set(inserts)
+    def candidates(self, word):
+        "Generate possible spelling corrections for word."
+        # return (self.__known([word]) | self.__known(self.__edits1(word)) | self.__known(self.__edits2(word)) | self.__known(self.__edits1Expanded(word)) | {word})
+        return (self.__known([word]) | self.__known(self.__edits1(word)) | self.__known(self.__edits2(word)) | {word})
 
-def edits2(word):
-    "All edits that are two edits away from `word`."
-    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+    def __known(self, words):
+        "The subset of `words` that appear in the dictionary of WORDS."
+        return set(w for w in words if w in self.counter)
 
-def edits1Expanded(word):
-    return (e3 for e1 in edits1(word) for e2 in edits1(e1) for e3 in edits1(e2))
+    def __edits1(self, word):
+        "All edits that are one edit away from `word`."
+        letters    = 'abcdefghijklmnopqrstuvwxyz'
+        splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+        # deletes    = [L + R[1:]               for L, R in splits if R]
+        # transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+        # replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+        inserts    = [L + c + R               for L, R in splits for c in letters]
+        # return set(deletes + transposes + replaces + inserts)
+        return set(inserts)
+
+    def __edits2(self, word):
+        "All edits that are two edits away from `word`."
+        return (e2 for e1 in self.__edits1(word) for e2 in self.__edits1(e1))
+
+    def __edits1Expanded(self, word):
+        return (e3 for e1 in self.__edits1(word) for e2 in self.__edits1(e1) for e3 in self.__edits1(e2))
+
+    def save(self):
+        pickle.dump( self.words, open( "pickled/_spell_words.p", "wb" ) )
+        pickle.dump( self.counter, open( "pickled/_spell_counter.p", "wb" ) )
+
+    def validate(self, sentence, debug=False):
+        valid = []
+
+        translator = str.maketrans({key: None for key in string.punctuation})
+        words = [token.translate(translator).strip() for token in sentence.split()]
+
+        # print(str(words))
+        for idx, word in enumerate(words):
+            if word in self.words:
+                valid.append(word.lower())
+            else:
+                if idx >= 2:
+                    candidates = self.candidates(word.lower())
+                    max_word = max([w for w in candidates], key=lambda w : self.model.sentence_prob(valid[idx - 2] +' '+ valid[idx - 1] +' '+ w))
+                    valid.append(max_word)
+
+                    if debug:
+                        print('candidates for '+ word +': '+ str(candidates) +', max prob word is '+ max_word.lower())
+                else:
+                    valid.append(self.correction(word))
+            # print('find max prob for candidates '+ word.lower())
+            # if idx >= 2:
+            #     valid.append(max([w for w in self.candidates(word.lower())], key=lambda w : self.model.sentence_prob(valid[idx - 2] +' '+ valid[idx - 1] +' '+ w)))
+            # else:
+            #     valid.append(self.correction(word))
+        return ' '.join(valid)
