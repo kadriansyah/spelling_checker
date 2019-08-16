c9b09d55cbdb946376ea7bd0e7094d7947841922
Kiagus Arief Adriansyah <arief@alodokter.com>
2019-08-16 14:40:01 +0700

    adding test case

--- test_word2vec.py
+++ test_word2vec.py
@@ -1,12 +1,33 @@
 from phrase_vector import PhraseVector
 from gensim.models import KeyedVectors
 
+corpus_file  = 'corpus/questions/corpus.txt'
+result_file  = 'test/test_result-2.txt'
+
 model = KeyedVectors.load('models/word2vec.model')
 
-s1 = 'dok apakah normal jika wanita orgasme tapi tidak mengeluarkan cairan kental seperti sperma'
-s2 = 'dok apakah normal jika wanita orgasme tapi tidak mengeluarkan cairan kental seperti sperma'
+# s1 = 'dok apakah normal jika wanita orgasme tapi tidak mengeluarkan cairan kental seperti sperma'
+# s2 = 'dok apakah normal jika wanita orgasme tapi tidak mengeluarkan cairan kental seperti sperma'
 
+# phraseVector1 = PhraseVector(model, s1)
+# phraseVector2 = PhraseVector(model, s2)
+# similarityScore  = phraseVector1.CosineSimilarity(phraseVector2.vector)
+# print("Similarity Score: ", similarityScore)
+
+f = open(result_file,"w+")
+
+# s1 = 'dok apakah normal jika wanita orgasme tapi tidak mengeluarkan cairan kental seperti sperma'
+s1 = 'dok saya sedang hamil 9 minggu bolehkah saya minum air kelapa muda kelapa hijau terima kasih'
 phraseVector1 = PhraseVector(model, s1)
-phraseVector2 = PhraseVector(model, s2)
-similarityScore  = phraseVector1.CosineSimilarity(phraseVector2.vector)
-print("Similarity Score: ", similarityScore)
+for sentence in open(corpus_file):
+    phraseVector2 = PhraseVector(model, sentence)
+    similarityScore  = phraseVector1.CosineSimilarity(phraseVector2.vector)
+    if similarityScore >= 0.95:
+        print("sentence-1: {}".format(s1))
+        print("sentence-2: {}".format(sentence))
+        print("Similarity Score: {}\r\n\r\n".format(similarityScore))
+
+        f.write("sentence-1: {}".format(s1))
+        f.write("sentence-2: {}".format(sentence))
+        f.write("Similarity Score: {}\r\n\r\n".format(sentence))
+f.close()
